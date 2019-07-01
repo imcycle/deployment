881431be88c847650a01da38ac17719537cc795b
whosmeya <whosmeya@gmail.com>
2019-07-02 01:19:23 +0800

    update

--- public/shell/blog_react_ts.sh
+++ public/shell/blog_react_ts.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
+echo '--start--'
 cd /root/code/blog_react_ts
 git checkout master
 git pull
 npm install
 npm run build
 mv /root/code/blog_react_ts/build /root/libs/blog_react_ts/
+echo '--end--'
