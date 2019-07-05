eefd9d44e2ff86bd8e4cc35eea77bdf88dc012bb
caoyucong <whosmeya@gmail.com>
2019-07-05 17:12:36 +0800

    update

--- public/shell/whosmeya-landing-page.sh
+++ public/shell/whosmeya-landing-page.sh
@@ -4,5 +4,5 @@ cd /root/code/whosmeya-landing-page
 git checkout master
 git pull
 rm -rf /root/libs/whosmeya-landing-page/*
-cp /root/code/whosmeya-landing-page/* /root/libs/whosmeya-landing-page/
+cp -rf /root/code/whosmeya-landing-page/* /root/libs/whosmeya-landing-page
 echo '--build end--'
