fdaffc633033079ce39abf85a73170a8a4842b8f
caoyucong <whosmeya@gmail.com>
2019-07-05 16:50:54 +0800

    update

--- public/shell/blog_react_ts.sh
+++ public/shell/blog_react_ts.sh
@@ -5,6 +5,6 @@ git checkout master
 git pull
 npm install
 npm run build
-rm -rf /root/libs/blog_react_ts/
+rm -rf /root/libs/blog_react_ts/*
 mv /root/code/blog_react_ts/build /root/libs/blog_react_ts/
 echo '--end--'
--- public/shell/whosmeya-landing-page.sh
+++ public/shell/whosmeya-landing-page.sh
@@ -3,6 +3,6 @@ echo '--build start--'
 cd /root/code/whosmeya-landing-page
 git checkout master
 git pull
-rm -rf /root/libs/whosmeya-landing-page/
-mv /root/code/whosmeya-landing-page/ /root/libs/whosmeya-landing-page/
+rm -rf /root/libs/whosmeya-landing-page/*
+mv /root/code/whosmeya-landing-page/* /root/libs/whosmeya-landing-page/
 echo '--build end--'
