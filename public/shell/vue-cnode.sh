432d5e33af4d571db770ba865f6d4f0156b20192
whosmeya <whosmeya@gmail.com>
2019-07-06 12:44:04 +0800

    modify vue-cnode.sh

--- public/shell/vue-cnode.sh
+++ public/shell/vue-cnode.sh
@@ -6,5 +6,5 @@ git pull
 npm install
 npm run build
 rm -rf /root/libs/vue-cnode/*
-mv /root/code/vue-cnode/build/* /root/libs/vue-cnode
+mv /root/code/vue-cnode/dist/* /root/libs/vue-cnode
 echo '--build end--'
