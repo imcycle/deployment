1e0eec1e9838f3470396a30291a3a81566bda392
whosmeya <whosmeya@gmail.com>
2019-07-14 23:44:38 +0800

    angular-cnode.sh modify local build

--- public/shell/angular-cnode.sh
+++ public/shell/angular-cnode.sh
@@ -4,8 +4,11 @@ cd /root/code/angular-cnode
 git checkout .
 git checkout master
 git pull
-npm install
-npm run build
+
+# 服务器内存太小 改为本地打包上传
+# npm install
+# npm run build
+
 rm -rf /root/libs/angular-cnode/*
 mv /root/code/angular-cnode/dist/angular-cnode/* /root/libs/angular-cnode
 echo '--build end--'
