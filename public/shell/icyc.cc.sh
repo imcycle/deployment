4b1e94ec3eab619f005ac0f520aac68fd3944fd0
caoyucong <caoyucong1@zto.com>
2021-04-28 17:11:34 +0800

    feat: icyc.cc添加部署并上传

--- public/shell/icyc.cc.sh
+++ public/shell/icyc.cc.sh
@@ -5,7 +5,7 @@ git checkout .
 git checkout master
 git pull
 npm install
-npm run build
+npm run build:update
 rm -rf /root/libs/icyc.cc/*
 cp /root/code/icyc.cc/dist/* /root/libs/icyc.cc
 echo '--build end--'
